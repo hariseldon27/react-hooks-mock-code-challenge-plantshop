bfe9799840222a711968fccc685248084ccc6cbd
Jonathan Potter <jonathan@mrjonathanpotter.com>
2022-02-08 08:41:27 -0800

    plant add button works to create new, need to re-render dom on add

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,13 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 function NewPlantForm() {
+  const [formData, setFormData] = useState({
+    name: "",
+    image: "url",
+    price: 0
+  })
+
+
+  function handleFormInput(event) {
+    const name = event.target.name
+    let value = event.target.value
+    setFormData({...formData, [name]: value})
+  }
+
+  function handleSubmit(event){
+    event.preventDefault()
+    const newPlantToAdd = {
+      name: formData.name,
+      image: formData.image,
+      likes: formData.price
+    }
+    fetch("http://localhost:6001/plants", {
+      method: "POST",
+      headers: { "Content-Type": "application/json",},
+      body: JSON.stringify(newPlantToAdd),
+    })
+    .then((resp) => resp.json())
+    .then((newlyAddedPlant) => console.log(newlyAddedPlant)
+    )}
+
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
-      <form>
-        <input type="text" name="name" placeholder="Plant name" />
-        <input type="text" name="image" placeholder="Image URL" />
-        <input type="number" name="price" step="0.01" placeholder="Price" />
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" placeholder="Plant name" onChange={handleFormInput}/>
+        <input type="text" name="image" placeholder="Image URL" onChange={handleFormInput}/>
+        <input type="number" name="price" step="0.01" placeholder="Price" onChange={handleFormInput}/>
         <button type="submit">Add Plant</button>
       </form>
     </div>
